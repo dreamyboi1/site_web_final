80db26b40e348f55fa876549579f770962bf927b
dreamyboi1 <oskar.girardin@web.de>
2022-03-03 11:28:24 +0100

    Supabase with new RLS-policy. Now every user can read the data

--- src/components/form.svelte
+++ src/components/form.svelte
@@ -2,12 +2,12 @@
     import supabase from "$lib/db.js"
 
     async function getData() {
-        let { data: games, error } = await supabase
-            .from('games')
-            .select("title")
+        let { data, error } = await supabase
+            .from('products')
+            .select()
         if (error) throw new Error(error.message)
-        
-        return games
+        console.log(data)
+        return data
     }
 
     let newGame
@@ -24,7 +24,6 @@
     }
 </script>
 
-<!--
 <form>
     <p>Potential title</p>
     <legend for="email">Your E-Mail:</legend>
@@ -36,28 +35,14 @@
 </form>
 
 
-<form on:submit|preventDefault={() => submit = true}>
-    <input type="text" bind:value={newGame}>
-    <input type="submit" value="Submit" on:click={() => submit = false}>
-  </form>
-  {#if submit}
-    {#await sendData()}
-      <p>Sending data...</p>
-    {:then data}
-      <p>Successfully sent data.</p>
-    {:catch error}
-      <p>Something went wrong while sending the data:</p>
-      <pre>{error}</pre>
-    {/await}
-  {/if}
--->
+
 
 <h1>My favorite games</h1>
 {#await getData()}
   <p>Fetching data...</p>
 {:then data}
-  {#each data as game}
-    <li>{game.title}</li>
+  {#each data as product}
+    <li>{product.id}</li>
   {/each}
 {:catch error}
   <p>Something went wrong while fetching the data:</p>

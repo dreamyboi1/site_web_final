3f33f9cfe3f611ca792fc626d8484776ef66ed12
Elcomiqu321 <armandpicard321@gmail.com>
2022-03-07 22:13:19 +0100

    Nouvelle modif sur Product

--- src/routes/products.svelte
+++ src/routes/products.svelte
@@ -9,23 +9,29 @@ import Cart from "./cart.svelte";
     <div class="flex justify-center flex-wrap">
 
         <div class="mx-4 mb-2 rounded-lg shadow-lg bg-slate-500 w-1/4 min-w-[200px] max-w-[300px]">
+
             <img class="w-full h-48 rounded-t-lg"
                 src="../static/placeholder.png"
                 alt="product1" />
             <div class="px-6 py-4">
                 <h4 class="text-center mb-3 text-xl font-semibold tracking-tight text-white">Product 1</h4>
                 <p class="leading-normal mb-3 text-gray-900 text-justify">This is a smol description to briefly understand the product.</p>
-                <div>
-                    <h4 class="text-center mb-3 text-xl font-semibold tracking-tight text-yellow-600">100.00 €</h4>
-                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                        <img class="w-4 h-4 mr-2"
-                        src="../static/shopping-cart.png" 
-                        alt="add to cart"/>
-                        <span class="text-red-700">+</span>
-                  </button>
-                </div>
-                
             </div>
+
+            <div class="px-6 py-4">
+                <p class="underline text-blue-400 leading-normal mb-3 text-justify">See product info</p>
+            </div>
+
+            <div class="flex justify-between gap-6">
+                <h4 class="text-center mb-3 px-4 text-xl font-semibold tracking-tight text-yellow-600">100.00 €</h4>
+                <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold m-3 py-2 px-4 rounded inline-flex items-center">
+                    <img class="w-4 h-4 mr-2"
+                    src="../static/shopping-cart.png" 
+                    alt="add to cart"/>
+                    <span class="text-white">+</span>
+                </button>
+            </div>
+
         </div>
 
         <div class="flex justify-center flex-wrap">

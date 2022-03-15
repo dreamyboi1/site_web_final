018de61d5dd6239377dac9052e1400ce347010a8
Elcomiqu321 <armandpicard321@gmail.com>
2022-03-15 22:43:40 +0100

    commencé page team

---
+++ src/components/team_widget.svelte
@@ -0,0 +1,46 @@
+
+
+
+<div class="w-1/2 min-w-[100px] max-w-[300px] group rounded-lg shadow-lg border-2 border-solid border-transparent bg-slate-100 hover:bg-slate-800 hover:hover:border-yellow-500">
+    <div class="relative pb-3/5 rounded-lg shadow-lg m-3">
+        <img class="absolute w-full h-full object-cover rounded-lg"
+        src="../static/placeholder.png"
+        alt="product1" />
+    </div>
+    <div class="bg-slate-800 text-white group-hover:bg-white group-hover:text-slate-800 p-4 rounded-lg shadow-lg m-3">
+        <h4 class="text-center pb-2 text-xl font-semibold tracking-tight">Name</h4>
+        <p class="leading-normal pb-2 text-justify">His role</p>
+    
+        <table class="text-center w-full table-auto ">
+            <tbody>
+                <tr>
+                    <td>Weight</td>
+                    <td class="text-red-700">12.5 kg</td>
+                </tr>
+                <tr>
+                    <td>Size</td>
+                    <td class="text-red-700">12 cm</td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td class="text-red-700">57 y/o</td>
+                </tr>
+                <tr>
+                    <td>Warranty</td>
+                    <td class="text-red-700">666 years</td>
+                </tr>
+                <tr>
+                    <td>Autonomy</td>
+                    <td class="text-red-700">12 months</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+
+    <div class="flex justify-between items-center px-12 py-4">
+        <h4 class="text-center text-xl font-semibold tracking-tight mb-7
+        sm:mb-1 text-yellow-600">test</h4>
+    </div>
+
+</div>
--- src/routes/team.svelte
+++ src/routes/team.svelte
@@ -1,11 +1,37 @@
 <script>
     import Nav from "../components/nav.svelte";
+    import TeamWidget from "../components/team_widget.svelte";
+
+
+    let nbStaff = 8;
+
 </script>
 
-<Nav/>
+<div class="sticky z-10 top-0 left-0">
+    <Nav></Nav>
+</div>
 
-<h1 class="text-center font-semibold tracking-tight text-3xl">
-    Team
-</h1>
+<body class="bg-gray-900">
+    <div class="grid place-items-center py-12">
+        <div class="w-1/2">
+            <h1 class="text-center font-semibold tracking-tight text-3xl">
+                Here is our marvelous team!
+            </h1>
+        </div>
+    </div>
+
+    <div class="flex justify-center flex-wrap gap-6">
+
+        {#each Array(nbStaff) as _, index}
+
+            <TeamWidget></TeamWidget>
+        
+
+        {/each}
+
+    </div>
+
+
+</body>
 
 

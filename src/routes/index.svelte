cb0d038e5b3cd19473204002bc18060ba257aa8a
dreamyboi1 <oskar.girardin@web.de>
2022-03-18 23:16:31 +0100

    Changed color theme. Not final yet

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,32 +9,32 @@
 <Nav/>
 
 
-<section id="hero_text" class="grid place-items-center">
+<section id="hero_text" class="grid place-items-center bg-pink-500">
 
     <div class="p-8 grid max-w-7xl place-items-center gap-8 grid-cols-2 lg:grid-cols-5 lg:grid-rows-3 sm:grid-cols-3">
-        <p class="text-4xl text-teal-400 font-extrabold col-span-2 lg:col-span-3">
+        <p class="text-4xl text-yellow-300 font-extrabold col-span-2 lg:col-span-3">
             Useless market.
-            A selection of 15 products that have one purpose: <span class="text-lime-400">None.</span>
+            A selection of 15 products that have one purpose: <span class="text-blue-500">None.</span>
         </p>
             
-        <img class="lg:col-start-5 aspect-square object-cover rounded-md" src="/static/img_index/fork1.jpg" alt="A concrete umbrella" >
-        <img class="lg:col-start-2 aspect-square object-cover rounded-md" src="/static/img_index/concrete_umbrella.jpg" alt="A concrete umbrella" >
-        <img class="aspect-square object-cover rounded-md hover:scale-125" src="/static/img_index/thick_button.jpg" alt="A concrete umbrella" >
-        <img class="aspect-square object-cover rounded-md hover:scale-125" src="/static/img_index/long_mug.jpg" alt="A concrete umbrella" >
-        <img class="aspect-square object-cover rounded-md hover:scale-125" src="/static/img_index/rain_boots.jpg" alt="A concrete umbrella" >
-        <img class="aspect-square object-cover rounded-md hover:shadow-lg hover:shadow-red-300" src="/static/img_index/fork2.jpg" alt="A concrete umbrella" >
-        <img class="aspect-square object-cover rounded-md" src="/static/img_index/two-pints.jpg" alt="A concrete umbrella" >
-
-        <img class="aspect-square object-cover rounded-md" src="/static/img_index/uncomfortable_teapot.jpg" alt="A concrete umbrella" >
-
-        <p class="sm:self-center col-span-2">
+        <img class="lg:col-start-5 aspect-square object-cover rounded-md shadow-xl" src="/static/img_index/concrete_umbrella.jpg" alt="A concrete umbrella" >
+        <p class="sm:self-center col-span-2 text-yellow-300  font-semibold">
             Lorem ipsum dolor sit amet consectetur 
         </p>
+        <img class="aspect-square object-cover rounded-md" src="/static/img_index/uncomfortable_teapot.jpg" alt="A concrete umbrella" >
+        <img class="aspect-square object-cover rounded-md hover:scale-125" src="/static/img_index/long_mug.jpg" alt="A concrete umbrella" >
+        <img class="aspect-square object-cover rounded-md hover:scale-125" src="/static/img_index/thick_button.jpg" alt="A concrete umbrella" >
+        <img class="lg:col-start-2 aspect-square object-cover rounded-md hover:scale-125" src="/static/img_index/rain_boots.jpg" alt="A concrete umbrella" >
+        <img class="aspect-square object-cover rounded-md" src="/static/img_index/fork1.jpg" alt="A concrete umbrella" >
+        <img class="aspect-square object-cover rounded-md hover:shadow-lg hover:shadow-red-300" src="/static/img_index/pedal_less_bicycle.jpg" alt="A concrete umbrella" >
+        <img class="aspect-square object-cover rounded-md" src="/static/img_index/slide.jpg" alt="A concrete umbrella" >
+
+
     </div>
 
 </section>
 <span class="w-full "> </span>
-<section id="history" class="w-full flex flex-col divide-teal-300">
+<section id="history" class="w-full flex flex-col bg-pink-500 text-yellow-300">
     <p class="text-4xl font-semibold mx-auto p-6">History of Useless Design</p>
     <p class="text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas consequatur quasi illo, possimus assumenda odio totam molestiae inventore aspernatur quos! Similique quisquam nemo consequatur, sunt minus iste, officia deserunt iusto commodi consectetur vitae nisi sequi! Hic soluta corporis possimus aspernatur.</p>
 </section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,23 @@
 
 module.exports = {
 theme: {
+  /* colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'main': '#afd5eb',
+      'black': '#111827',
+      'red': '#f04520',
+      'pink': '#ef869c',
+      'blue': '#107305',
+      'yellow': '#fde047',
+      'emerald': '#6ee7b7',
+      'violet': '#5b21b6',
+      'purple': '#d8b4fe',
+      'stone-800': '#292524',
+      'stone-900': '#1c1917',
+      'teal': '#5eead4',
+      'white': "#ffffff",
+  }, */
   extend: {
     spacing: {
       

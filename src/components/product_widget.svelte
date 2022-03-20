cf66544cdf311a9d4382713b1b29f31a0b3519ab
Elcomiqu321 <armandpicard321@gmail.com>
2022-03-20 23:20:47 +0100

    product button

--- src/components/product_widget.svelte
+++ src/components/product_widget.svelte
@@ -18,20 +18,21 @@
         const { data, error } = await supabase
             .from('cart')
             .insert([
-            { 'user_id': $user.id, 'product_id' : 99, 'quantity' : 1}
+            { 'user_id': $user.id, 'product_id' : $productId, 'quantity' : 23}
         ])
-        if (error) throw new Error(error.message)
+        if (error) throw new Error(error.message) 
     }
+    
 
 </script>
 
-    <div class="w-1/2 min-w-[100px] max-w-[300px] group rounded-lg shadow-lg border-2 border-solid border-transparent bg-slate-100 hover:bg-slate-800 hover:hover:border-yellow-500">
+    <div class="w-1/2 min-w-[100px] max-w-[300px] rounded-lg shadow-lg bg-slate-100">
         <div class="relative pb-3/5 rounded-lg shadow-lg m-3">
             <img class="absolute w-full h-full object-cover rounded-lg"
             src={imgUrl}
             alt={title} />
         </div>
-        <div class="bg-blue-500 text-white group-hover:bg-white group-hover:text-slate-800 p-4 rounded-lg shadow-lg m-3">
+        <div class="bg-blue-500 text-white p-4 rounded-lg shadow-lg m-3">
             <h4 class="text-center pb-2 text-xl font-semibold tracking-tight">{title}</h4>
             <p class="leading-normal pb-4 text-justify ">{description}</p>
         
@@ -62,11 +63,10 @@
             </table>
         </div>
 
-        <div class="flex justify-between items-center px-12 py-4">
+        <div class="flex justify-between items-center px-12 py-4 flex-wrap">
             <h4 class="text-center text-xl font-semibold tracking-tight mb-7
             sm:mb-1 text-yellow-600">{price} €</h4>
-            <button class="shadow-lg bg-blue-500 hover:bg-gray-300 text-white font-bold mb-3 md:mb-2 sm:mb-1 py-2 px-4 rounded inline-flex items-center"
-            on:click={sendData()}>
+            <button class="shadow-lg bg-blue-500 hover:bg-gray-300 text-black font-bold mb-3 md:mb-2 sm:mb-1 py-2 px-4 rounded inline-flex items-center">
                 <img class="w-4 h-4 mr-2"
                 src="../static/shopping-cart.png" 
                 alt="add to cart"/>
--- src/components/team_widget.svelte
+++ src/components/team_widget.svelte
@@ -9,18 +9,18 @@
 </script>
 
 
-<div class="w-1/2 min-w-[100px] max-w-[300px] group rounded-lg rounded-t-3xl shadow-lg border-2 border-solid border-transparent bg-blue-500 hover:bg-slate-800 hover:border-yellow-500">
+<div class="w-1/2 min-w-[100px] max-w-[300px] rounded-lg rounded-t-3xl shadow-lg bg-white">
     <div class="relative pb-3/5 rounded-full shadow-lg m-3">
         <img class="absolute w-full h-full object-cover rounded-full"
         src={urlImgTeam}
         alt={nameT} />
     </div>
-    <div class="bg-slate-500 text-white group-hover:bg-white group-hover:text-slate-800 p-4 rounded-lg shadow-lg m-3">
+    <div class="bg-blue-500 text-white p-4 rounded-lg shadow-lg m-3">
         <h4 class="text-center pb-2 text-xl font-semibold tracking-tight">{nameT}</h4>
         <p class="leading-normal italic pb-2 text-justify">{roleT}</p>
     </div>
 
-    <div class="bg-slate-500 text-white group-hover:bg-white group-hover:text-slate-800 p-4 rounded-lg shadow-lg m-3">
+    <div class="bg-blue-500 text-white p-4 rounded-lg shadow-lg m-3">
         <h4 class="text-center text-xl font-semibold tracking-tight mb-7
         sm:mb-1 text-yellow-500">{quote}</h4>
     </div>
